1961d80df0b2b6bb13983efac0f9224651037a29
viveksoni100 <viveksoni100@gmail.com>
2021-06-23 13:02:56 +0530

    actuator demo project is added

---
+++ AcuatorDemo/src/main/java/com/example/AcuatorDemo/AcuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AcuatorDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcuatorDemoApplication.class, args);
+	}
+
+}
---
+++ AcuatorDemo/src/test/java/com/example/AcuatorDemo/AcuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AcuatorDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
